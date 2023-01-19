7349d3e2ec7a6cd52ec1c01b39e51d1fe2950f28
egotessy <123082539+egotessy@users.noreply.github.com>
2023-01-19 14:04:25 +0100

    Add files via upload

---
+++ Brainteaser2.py
@@ -0,0 +1,31 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=500
+>>> print(type(x))
+<class 'int'>
+>>> 
+>>> y="bulabah"
+>>> print(type(y))
+<class 'str'>
+>>> 
+>>> z=3.142
+>>> print(type(z))
+<class 'float'>
+>>> 
+>>> is_valid=True
+>>> print(type(is valid))
+SyntaxError: invalid syntax
+>>> 
+>>> is_valid=True
+>>> print(type(is_valid))
+<class 'bool'>
+>>> 
+>>> x=5
+>>> print(isinstance(x, int))
+True
+>>> y=100.9
+>>> print(isinstance(x,float))
+False
+>>> z=1.003
+>>> print(isinstance(z,float))
+True
