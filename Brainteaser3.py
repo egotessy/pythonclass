804ad63532bb30faedb98ac61ad0f23244d0a44e
egotessy <123082539+egotessy@users.noreply.github.com>
2023-01-19 14:05:09 +0100

    Add files via upload

---
+++ Brainteaser3.py
@@ -0,0 +1,27 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> StudentID="Name"
+>>> print(isinstance(StudentID,str))
+True
+>>> 
+>>> Gender="male/Female"
+>>> print(isinstance(Gender,str))
+True
+>>> 
+>>> Age=18
+>>> print(isinstance(Age,int))
+True
+>>> 
+>>> Eligible=True
+>>> print(type(Eligible))
+<class 'bool'>
+>>> print(isinstance(Eligible,str))
+False
+>>> print(isinstance(True,str))
+False
+>>> 
+>>> Marks=70.9
+>>> print(isinstance(Marks,float))
+True
+>>> 
